1a5dadd10a1a98547e1ed4c647a40c089109250e
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-02-28 13:17:58 -0700

    fixed the \n for appending the csv

--- High Score Tracker/simple_game_main.py
+++ High Score Tracker/simple_game_main.py
@@ -10,14 +10,14 @@ def simple_game_main():
                 score, oneTen, username = ten()
                 if 'end' in oneTen:
                     with open('High Score Tracker/simple_game1-10.csv','a') as file:
-                        file.write(f"{username},{score}") 
+                        file.write(f"\n{username},{score}") 
                     break       
         elif choice == '2':
             while True:
                 score, oneHundred, username = hundred()
                 if 'end' in oneHundred:
                     with open('High Score Tracker/simple_game1-100.csv','a') as file:
-                        file.write(f"{username},{score}")
+                        file.write(f"\n{username},{score}")
                     break
         elif choice == '3':
             break
