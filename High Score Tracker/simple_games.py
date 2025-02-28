bf44bbaea36adb3678e007f34d62c40668138413
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-28 13:20:21 -0700

    added comments

--- High Score Tracker/simple_games.py
+++ High Score Tracker/simple_games.py
@@ -8,7 +8,7 @@ def save_user(userName):
         userName = save_user(input("Invalid username, what is your username? (Please make it 3 characters) ").upper())
         return userName
 
-def one_ten():
+def one_ten(): #The user guesses from 1-10
     score = 0
     current = 0
     streak = 0
@@ -16,6 +16,7 @@ def one_ten():
     while True:
             comp_num = random.randint(1,10)
             while True:
+                #Error handling for making sure the user chooses a number
                 try:
                     user_num = int(input("\nGuess a number from 1-10!\n      "))
                     if user_num >= 1 and user_num <= 10:
@@ -26,9 +27,10 @@ def one_ten():
                 except ValueError:
                     print("Please choose a valid NUMBER.")
                     continue
-                if user_num == comp_num:
+                if user_num == comp_num: #Checks to see if the user picked the right number
                     print("\n--------------CONGRATTS!!!! YOU DID IT----------------\n")
                     streak+=1
+                    #Depending on how many times it takes for the user to get the right answer, they can get a score increase or boost
                     if current <= 3 and current > 0:
                         score = score+streak*2
                         current = 0
@@ -40,7 +42,7 @@ def one_ten():
                     while True:
                         play = input(f"SCORE: {score}\nSTREAK: {streak}\nWould you like to keep playing?(yes or no)\n ").title()
                         if play == 'No':
-                            return score, 'end', username
+                            return score, 'end', username 
                         elif play == 'Yes':
                             break
                         elif play != 'Yes' and play != 'No':
@@ -48,6 +50,7 @@ def one_ten():
                             continue
                     break
                 elif user_num != comp_num:
+                    #Checks to see if the user needs to guess higher or lower
                     if user_num > comp_num:
                         print("Go a bit lower!")
                     elif user_num < comp_num:
@@ -58,14 +61,15 @@ def one_ten():
                     continue
 
 
-def one_hundred():
+def one_hundred(): #The user guesses 1-100
     score = 0
     current = 0
     streak = 0
-    username = save_user(input("What is your username? (Please make it 3 characters) ").upper())
+    username = save_user(input("What is your username? (Please make it 3 characters) ").upper()) #Gets username from user
     while True:
             comp_num = random.randint(1,100)
             while True:
+                #Error handling for making sure the user chooses a number
                 try:
                     user_num = int(input("\nGuess a number from 1-100!\n      "))
                     if user_num >= 1 and user_num <= 100:
@@ -79,6 +83,7 @@ def one_hundred():
                 if user_num == comp_num:
                     print("\n--------------CONGRATTS!!!! YOU DID IT----------------\n")
                     streak+=1
+                    #Depending on how many times it takes for the user to get the right answer, they can get a score increase or boost
                     if current <= 3 and current > 0:
                         score = score+streak*2
                         current = 0
@@ -98,6 +103,7 @@ def one_hundred():
                             continue
                     break
                 elif user_num != comp_num:
+                    #Checks to see if the user needs to guess higher or lower
                     if user_num > comp_num:
                         print("Go a bit lower!")
                     elif user_num < comp_num:
